68fad022ee660368665f65a7ca63cddc222c1f1c
Niqrs <niqr.dev@gmail.com>
2023-03-28 10:29:12 +0300

    Hilt initialization

---
+++ app/src/main/java/com/niqr/mono/di/AppModule.kt
@@ -0,0 +1,24 @@
+package com.niqr.mono.di
+
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    fun provideFirebaseAuth() = Firebase.auth
+
+    @Provides
+    fun provideFirebaseFirestore() = Firebase.firestore
+
+    @Provides
+    fun provideFirebaseStorage() = Firebase.storage
+}
