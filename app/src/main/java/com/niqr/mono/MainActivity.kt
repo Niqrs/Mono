e539d18df4344b4abac20af8a9e2d3753e126592
Niqrs <niqr.dev@gmail.com>
2023-03-29 07:15:45 +0300

    Create MonoNavigation

--- app/src/main/java/com/niqr/mono/MainActivity.kt
+++ app/src/main/java/com/niqr/mono/MainActivity.kt
@@ -3,13 +3,6 @@ package com.niqr.mono
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.niqr.mono.ui.theme.MonoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,24 +12,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MonoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+                MonoNavigation()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MonoTheme {
-        Greeting("Android")
     }
 }
