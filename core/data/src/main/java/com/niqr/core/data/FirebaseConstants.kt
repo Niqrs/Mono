b11d0553a1a1400179313bf9004a77aebe4448b1
Niqrs <niqr.dev@gmail.com>
2023-04-03 19:55:03 +0300

    Add new FirebaseConstants

--- core/data/src/main/java/com/niqr/core/data/FirebaseConstants.kt
+++ core/data/src/main/java/com/niqr/core/data/FirebaseConstants.kt
@@ -9,4 +9,8 @@ object FirebaseConstants {
     const val DISPLAY_NAME = "displayName"
     const val EMAIL = "email"
     const val PHOTO_URL = "photoUrl"
+
+    const val DAYS = "days"
+    const val DAY = "day"
+    const val TASKS = "tasks"
 }
