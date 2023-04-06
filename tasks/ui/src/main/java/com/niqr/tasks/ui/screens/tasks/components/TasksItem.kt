bc07a98e34907575ee51833d69e3e67c66878356
Niqrs <niqr.dev@gmail.com>
2023-04-06 19:30:50 +0300

    Update TasksItem

--- tasks/ui/src/main/java/com/niqr/tasks/ui/screens/tasks/components/TasksItem.kt
+++ tasks/ui/src/main/java/com/niqr/tasks/ui/screens/tasks/components/TasksItem.kt
@@ -3,11 +3,13 @@ package com.niqr.tasks.ui.screens.tasks.components
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.niqr.core.ui.theme.task
 import com.niqr.tasks.domain.model.Task
@@ -22,12 +24,15 @@ internal fun TasksItem(
     Text(
         text = "${task.name}: ${task.isDone}",
         modifier = Modifier
+            .fillMaxWidth()
             .combinedClickable(
                 onClick = { onClick(task.copy(isDone = !task.isDone)) },
                 onLongClick = { onLongClick(task) }
             )
             .basicMarquee()
             .padding(vertical = 10.dp),
+        textAlign = TextAlign.Center,
+        maxLines = 1,
         style = MaterialTheme.typography.task
     )
 }
