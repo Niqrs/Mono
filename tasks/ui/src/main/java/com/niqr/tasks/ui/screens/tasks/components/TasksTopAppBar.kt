b8c0922df17057a7fee97c6b8f99fc250c3b9d78
Niqrs <niqr.dev@gmail.com>
2023-04-07 20:30:20 +0300

    Update TasksTopAppBar

--- tasks/ui/src/main/java/com/niqr/tasks/ui/screens/tasks/components/TasksTopAppBar.kt
+++ tasks/ui/src/main/java/com/niqr/tasks/ui/screens/tasks/components/TasksTopAppBar.kt
@@ -1,52 +1,46 @@
 package com.niqr.tasks.ui.screens.tasks.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.niqr.core.ui.theme.Black
 import com.niqr.core.ui.theme.White
 import com.niqr.core.ui.theme.day
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun TasksTopAppBar(
     day: String,
     onSettingsClick: () -> Unit
 ) {
-    Box(
-        modifier = Modifier
-            .background(Black)
-            .padding(horizontal = 16.dp, vertical = 8.dp)
-            .fillMaxWidth()
-    ) {
-        Text(
-            text = day,
-            modifier = Modifier
-                .align(Alignment.Center),
-            color = White,
-            style = MaterialTheme.typography.day
-        )
-
-        IconButton(
-            onClick = onSettingsClick,
-            modifier = Modifier
-                .align(Alignment.CenterEnd)
-        ) {
-            Icon(
-                imageVector = Icons.Default.Settings,
-                contentDescription = null,
-                tint = White
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = day,
+                color = White,
+                style = MaterialTheme.typography.day
             )
-        }
-    }
+        },
+        actions = {
+            IconButton(
+                modifier = Modifier
+                    .padding(end = 8.dp),
+                onClick = onSettingsClick
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Settings,
+                    contentDescription = null
+                )
+            }
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = Black,
+            titleContentColor = White,
+            actionIconContentColor = White
+        )
+    )
 }
