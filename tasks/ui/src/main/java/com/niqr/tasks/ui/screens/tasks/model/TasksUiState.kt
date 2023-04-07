b31237b1e1543a6fb2ce723bbbbfa5d11981ff7a
Niqrs <niqr.dev@gmail.com>
2023-04-07 22:30:55 +0300

    Add isSettingsMenuVisible state

--- tasks/ui/src/main/java/com/niqr/tasks/ui/screens/tasks/model/TasksUiState.kt
+++ tasks/ui/src/main/java/com/niqr/tasks/ui/screens/tasks/model/TasksUiState.kt
@@ -5,5 +5,6 @@ import com.niqr.tasks.domain.model.Task
 data class TasksUiState(
     val day: String = "",
     val tasks: List<Task> = emptyList(),
-    val editTask: Task? = null
+    val editTask: Task? = null,
+    val isSettingsMenuVisible: Boolean = false
 )
